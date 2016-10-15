63015e43a13e3150f5f1a1000d12a4ca27b4285b
Jacob Martin <jmartin@altimetrik.com>
2016-10-15 17:06:05 -0400

    index added

---
+++ moldexbrand/public/index.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mold Cleaner | Mold Remover | Moldex Brands</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="keywords" content="killing mold, kill mold, mold and mildew remediation, Moldex mold stain cleaner." />
+    <meta name="description" content="Moldex Brands bring the power of profesional grade mold cleaner and mold remover to your home.  Kill mold, remove mold stains and prevent mold from returning." />
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="https://www.moldexbrands.com/images/favicon.ico">
+    <!-- this styles only adds some repairs on idevices  -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Google fonts - witch you want to use - (rest you can just remove) -->
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+
+</head>
+<body>
+    <div id="app"></div>
+    <script src="bundle.js"></script>
+</body>
+</html>

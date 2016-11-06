d1a9be7f8bb8a4eba20f4c4ea161aa61f6171f8a
jacobmmartin <media636@gmail.com>
2016-11-06 08:32:03 -0500

    factors page

--- app/components/container/factors/factors.js
+++ app/components/container/factors/factors.js
@@ -1,11 +1,77 @@
 import React, {Component} from 'react';
+import PageTitle from "../../presentation/pageTitle/pageTitle";
 
 class Factors extends Component {
     render() {
         return (
             <div>
-                FACTORS
+            
+             <PageTitle  title="How it works?"/>
+            <div className="container">
+            <div className="content_fullwidth">
+                <div className="one_full">
+                    <div className="big_text1">Our method is <i>Kill, Clean, and Prevent</i>. It's simple really. We call it the "3 Factors in Mold Fighting™". It works because each proprietary formula targets a specific step in mold growth and builds on the previous.</div>
+                    <div className="clearfix margin_top2"></div>
+                    <div className="one_full center">
+                        <img src="https://www.moldexbrands.com/images/3_factors_lg.png" width="396" height="201" className="img_size2" alt=""/></div>
+                    
+                    <br/>
+                    <p>We know mold, and we know mildew. And we know what it takes to properly deal with this stuff and prevent it from coming back.
+                        <br/>
+                        <br/> We also know that mold begins with the spore, launched by a "perfect storm" of microscopic events - fungus, water, heat, energy and ultimately, a surface to grow on.
+                        <br/>
+                        <br/> The Moldex "3 Factors in Mold Fighting™" approach addresses all the events that lead to mold growth and provide you with the best results for each step. The system was formulated not only to kill and inhibit the growth of mold &amp; mildew, but also help diminish any evidence of past infestations, ugly staining or black spots and restore surfaces back to its original appearance. Here's how to get rid of mold and mildew: </p>
+                </div>
+                
+                <div className="clearfix"></div>
+                <div className="clearfix divider_dashed6"></div>
+                <div className="one_half"><img src="https://www.moldexbrands.com/images/mold_root_factor_1.jpg" alt="" className="img_size1"  style={{border:"1px solid black"}}/></div>
+                
+                <div className="one_half last">
+                    <h3 className=""><strong><span style={{color: "#7ab700"}}>Factor 1:</span></strong> Kill the Mold &amp; Mildew</h3>
+                    <p>Molds reproduce by means of tiny spores; the spores are invisible to the naked eye and float through the air. Molds begin growing when spores land on moist surfaces and find food. “Food” means any organic substance, such as wood, paper, carpet, food, plastic, vinyl, and insulation. It is important to understand that mold &amp; mildew are living organisms and in order to effectively remove something that grows, you must disrupt its life cycle (including its root structure) by using an EPA registered biocide that gets rid of mold.
+                        <br/>
+                        <br/>
+                        <a href="killers.html">View Factor 1 Products</a></p>
+                </div>
+                
+                <div className="clearfix margin_top6"></div>
+                <div className="one_half"><img src="https://www.moldexbrands.com/images/mold_root_factor_2.jpg" alt="" className="img_size1"  style={{border:"1px solid black"}}/></div>
+                
+                <div className="one_half last">
+                    <h3 className=""><strong><span style={{color: "#7ab700"}}>>Factor 2:</span></strong> Clean the Stain</h3>
+                    <p>After you kill the mold &amp; mildew, the roots leave behind root-remnants and ugly staining that can go deep below the surface. Ordinary cleaners maybe not be able to remove these tough mildew stains so a powerful mold stain lifter is required to do the job. Our mold stain cleaners utilize special surfactants, hydrogen peroxide and bleach to create deep-cleaning action that breaks apart stains, dissolving them quickly and easily, while removing the ugly mold/mildew stain color.
+                        <br/>
+                        <br/>
+                        <a href="stain_removers.html">View Factor 2 Products</a></p>
+                </div>
+                
+                <div className="clearfix margin_top6"></div>
+                <div className="one_half"><img src="https://www.moldexbrands.com/images/mold_root_factor_3.jpg" alt="" className="img_size1"  style={{border:"1px solid black"}}/></div>
+                
+                <div className="one_half last">
+                    <h3 className=""><strong><span style={{color: "#7ab700"}}>>Factor 3:</span></strong> Seal the Surface</h3>
+                    <p>Sealing the surface with a clear acrylic polymer coating will block mold &amp; mildew on the surface at microscopic levels. A ultra-thin layer of protection is created by bonding molecules that are far more dense than water molecules or mold particles. The result is a water-resistant, invisible, barrier that protects against mold &amp; mildew damage, staining, and discoloration. Factor 3 is our secret weapon in the fight against mold &amp; mildew because it aims to stop problems before they even happen.
+                        <br/>
+                        <br/>
+                        <a href="prevention.html">View Factor 3 Products</a></p>
+                </div>
+                
             </div>
+            <div className="clearfix divider_dashed6"></div>
+            <div className="one_full">
+                <p>Moldex<span className="sup">®</span> works because of our unique formulations and this overall regimented approach. Each proprietary formula targets a specific step in the mold growth process and builds on the previous step. Taken as a whole, the 3 factor system provides everything you need to kill mold, help reduce growth, and seal the surface so future mold doesn't take hold.
+                    <br/>
+                    <br/>
+                </p>
+            </div>
+            <div className="big_text1 center">Like professional <i>mold remediators</i>, we believe this is the best way to approach mold &amp; mildew.
+                <br/>
+                <strong><i>1.</i> KILL <i> 2.</i> CLEAN <i>3.</i> PREVENT</strong></div>
+        </div>
+            
+            </div>
+           
         );
     }
 }

cc91bcda23cc5f9dfb79b6b3cf669a1f36445fd0
gpuentes <gpuentes@altimetrik.com>
2016-11-11 17:35:03 -0500

    -Improve css for mold guide page.

--- app/components/container/moldGuide/MoldGuide.js
+++ app/components/container/moldGuide/MoldGuide.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PageTitle from "../../presentation/pageTitle/pageTitle";
 
+require("./moldGuide.css");
+
+
 
 const MoldGuide = () =>{
     return(
             <div>
-            <PageTitle  title="Mold & Nildew Guide"/>
-            <div className="container">
+              <PageTitle  title="Mold & Nildew Guide"/>
+              <div className="container">
                 <div className="content_fullwidth">
                         <div className="one_full">
                             <div className="big_text1">Mold &amp; Mildew Basic Training:</div>
@@ -16,50 +19,87 @@ const MoldGuide = () =>{
                                 <br/>
                                 <br/> Here’s a rundown on common household molds, where to look for them, and the ill effects they bring...</p>
                         </div>
-                        
+
                         <div className="clearfix"></div>
                         <div className="clearfix divider_dashed6"></div>
-                        <div className="one_third"><img src="images/mold_1.jpg" alt="" className="img_size1"/></div>
-                        
+                        <div className="one_third"><img src="./images/mold_1.jpg" alt="" className="img_size1"/></div>
+
                         <div className="two_third last">
                             <h3><strong>Alternaria</strong></h3>
                             <p>A large spore mold that can deposit in the nose, mouth and upper respiratory tract causing an allergic response. Indoors, it is often found in carpets, textiles, house dust and potentially damp areas like window frames and showers. It can also be found in plant soil.</p>
                         </div>
-                        
+
                         <div className="clearfix margin_top6"></div>
-                        <div className="one_third"><img src="https://www.moldexbrands.com/images/mold_2.jpg" alt="" className="img_size1"/></div>
-                        
+                        <div className="one_third"><img src="./images/mold_2.jpg" alt="" className="img_size1"/></div>
+
                         <div className="two_third last">
                             <h3><strong>Aspergillus</strong></h3>
                             <p>Usually found in warmer climates in areas of water damage or extreme dampness. Aspergillus species are also commonly found in house dust. Many species produce mycotoxins which may be associated with disease in humans and some animals. Also found in building materials and in fall leaves and other decomposing matter like compost piles.</p>
                         </div>
-                        
+
                         <div className="clearfix margin_top6"></div>
-                        <div className="one_third"><img src="https://www.moldexbrands.com/images/mold_3.jpg" alt="" className="img_size1"/></div>
-                        
+                        <div className="one_third"><img src="./images/mold_3.jpg" alt="" className="img_size1"/></div>
+
                         <div className="two_third last">
                             <h3><strong>Cladosporium</strong></h3>
                             <p>The most commonly identified outdoor fungus, but it can easily enter into the house through the HVAC and other airflow entryways. Cladosporium also has an indoor species that grows on textiles, wood and other porous, damp areas. Both indoor and outdoor species are triggers for hay fever and asthma symptoms. </p>
                         </div>
-                        
+
                         <div className="clearfix margin_top6"></div>
-                        <div className="one_third"><img src="https://www.moldexbrands.com/images/mold_4.jpg" alt="" className="img_size1"/></div>
-                        
+                        <div className="one_third"><img src="./images/mold_4.jpg" alt="" className="img_size1"/></div>
+
                         <div className="two_third last">
                             <h3><strong>Penicillium</strong></h3>
                             <p>A very common mold known to cause allergies, hay fever and asthma. Species may be found growing on wallpaper, wallpaper glue and decaying fabrics in water-damaged buildings or homes. It is also found in carpet and in interior fiberglass duct insulation. Some species can produce mycotoxins.</p>
                         </div>
-                        
+
                         <div className="clearfix margin_top6"></div>
-                        <div className="one_third"><img src="https://www.moldexbrands.com/images/mold_5.jpg" alt="" className="img_size1"/></div>
-                    
+                        <div className="one_third"><img src="./images/mold_5.jpg" alt="" className="img_size1"/></div>
+
                         <div className="two_third last">
                             <h3><strong>Stachybotrys</strong></h3>
                             <p>Pronounced (stack-ee-BOT-ris), this is an especially toxic black mold that produces airborne toxins (mycotoxins) that can cause serious breathing difficulties, memory and hearing loss, dizziness, flu-like symptoms and bleeding in the lungs. Stachybotrys requires excessive moisture to thrive (usually running water) and is a slimy black mold. Fortunately, stachybotrys is not found in homes as often as the other molds listed above.</p>
                         </div>
-                    
+
+                  </div>
+
+
+                  <div className="one_full">
+                    <div className="clearfix margin_top2"></div>
+                    <p>It is important to remember that not all black molds are SC and that SC does not always produce mycotoxins. While alive, SC is slimy and does not release many spores. Exposure is greatest when SC dries and spores or bits of the mold growth are released into the air.</p>
+                    <div className="clearfix divider_dashed6"></div>
+                    <div className="big_text1">Resources &amp; Links:
+                        <p><a href="https://www.epa.gov/mold/moldbasics.html">Mold Basics</a> - Environmental Protection Agency
+                            <br />
+                            <a href="https://www.moldbacteria.com/mold-types.html">Mold List</a> - Mold Bacteria &amp; Consulting Laboratories</p>
+                        <p><a href="https://www.cdc.gov/mold/cleanup.htm">Mold</a> - Center for Disease Control
+                            <br />
+                            <a href="https://www.cdph.ca.gov/programs/IAQ/Pages/IndoorMold.aspx">Indoor Mold</a> - California Dept of Health Services</p>
                     </div>
+                    <div className="clearfix margin_top2"></div>
+                    <div className="big_text1">Flood Support &amp; Information:
+                        <p><a href="https://www.ready.gov/floods">FEMA</a> - Federal Emergency Management Agency
+                            <br />
+                            <a href="https://www.redcross.org/prepare/disaster">Flood Safety</a> - American Red Cross
+                            <br />
+                            <a href="https://www.fsa.usda.gov/FSA/webapp?area=home&amp;subject=diap&amp;topic=landing">Disaster Assistance Programs</a> - Farm Service Agency
+                            <br />
+                        </p>
+                    </div>
+                    <div className="clearfix margin_top2"></div>
+                    <div className="big_text1">Cleaning &amp; Prevention:
+                        <p><a href="https://www.epa.gov/mold/moldguide.html">Mold Guide: A brief guide to Mold in your home</a> - Environmental Protection Agency
+                            <br />
+                            <a href="https://www.epa.gov/iaq/flood/index.html">Fact Sheet: Mold Cleanup after a flood</a> - Environmental Protection Agency
+                            <br />
+                            <a href="https://www.nchh.org/Portals/0/Contents/FloodCleanupGuide_screen_.pdf">Flood Cleanup Guide</a> - Center for Healthy Housing
+                            <br />
+                            <a href="https://www.ksre.ksu.edu/bookstore/pubs/mf2141.pdf">Controlling Mold Growth in your Home</a> - Kansas State University</p>
+                    </div>
+                    <div className="clearfix margin_top2"></div>
+                  </div>
             </div>
+            <div className="clearfix margin_top7"></div>
             </div>
     )
 }

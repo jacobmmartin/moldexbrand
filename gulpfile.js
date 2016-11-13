7dc8d12d9e98d5dc6685dec3ef40f93dd1cb89a3
jacobmmartin <media636@gmail.com>
2016-11-13 09:57:46 -0500

    french extract

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ var gulp = require('gulp');
 var l10n = require('gulp-l10n');
  
 var opts = {
- 
+  elements: ['title', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'small', 'a', 'button', 'strong', 'i'],
   attributes: [],
   directives: 'translate=yes',
   attributeSetter: 'translate-attrs'
 };
 
 gulp.task('extract-locales', function () {
-  return gulp.src('app/**/*.html')
+  return gulp.src('app/html-FR/**/*.html')
     .pipe(l10n.extract(opts))
     .pipe(gulp.dest('locales'));
 });
